b4ad432c9c80b03afc0853ac1cfab3dce598822a
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-12 17:02:11 +0430

    initlaized swup and fixed some bugs

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -1,5 +1,6 @@
 // Variables
 const loader = document.getElementById('loader');
+const swup = new Swup();
 
 // Adding Event Listener Of Load On Window Which Listens To Load Of Full Page Then Closes Loader Modal
 window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
