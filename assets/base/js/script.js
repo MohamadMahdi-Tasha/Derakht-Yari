d756b637522e48fdee049065cb6893d9a95606f0
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-12 23:27:12 +0430

    added lines of comments

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -8,15 +8,18 @@ const rightSideDarkBg = document.getElementById('right-side-dark-bg');
 // Adding Event Listener Of Load On Window Which Listens To Load Of Full Page Then Closes Loader Modal
 window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
 
+// Adding '/' Behind Href Of Each Anchor Tag
 document.querySelectorAll('a').forEach(item => {
     item.setAttribute('href',`/${item.href}`)
 })
 
+// A Function That Toggles Classes To Body And Nav
 function showOrHideNav() {
     nav.classList.toggle('show')
     document.body.classList.toggle('overflow-hidden');
 }
 
+// Calling 'showOrHideNav' Function On Dark Bg Of Mobile Nav And Navs Toggle And Key Of Escape On KeyBoard
 mobileNavToggler.addEventListener('click',  showOrHideNav)
 rightSideDarkBg.addEventListener('click', showOrHideNav)
 window.addEventListener('keydown', (event) => {if (event.key === 'Escape') {showOrHideNav()}})
