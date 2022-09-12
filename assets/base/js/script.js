814dd5b4a99e6af2be6660be6ff6fd74178d2ec2
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-12 23:21:05 +0430

    added js requeired dor nav

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -1,6 +1,9 @@
 // Variables
 const loader = document.getElementById('loader');
 const swup = new Swup();
+const mobileNavToggler = document.getElementById('mobile-nav-toggler');
+const nav = document.getElementById('right-side');
+const rightSideDarkBg = document.getElementById('right-side-dark-bg');
 
 // Adding Event Listener Of Load On Window Which Listens To Load Of Full Page Then Closes Loader Modal
 window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
@@ -8,3 +11,12 @@ window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'
 document.querySelectorAll('a').forEach(item => {
     item.setAttribute('href',`/${item.href}`)
 })
+
+function showOrHideNav() {
+    nav.classList.toggle('show')
+    document.body.classList.toggle('overflow-hidden');
+}
+
+mobileNavToggler.addEventListener('click',  showOrHideNav)
+rightSideDarkBg.addEventListener('click', showOrHideNav)
+window.addEventListener('keydown', (event) => {if (event.key === 'Escape') {showOrHideNav()}})
