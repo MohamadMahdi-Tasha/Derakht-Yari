46d0587b46d48e2ace64131ab4e2baff94017712
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-12 19:55:05 +0430

    added styles of middle section

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -4,4 +4,8 @@ const swup = new Swup();
 
 // Adding Event Listener Of Load On Window Which Listens To Load Of Full Page Then Closes Loader Modal
 // window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
-setInterval(() => loader.setAttribute('data-opened', 'false'), 3000)
+setTimeout(() => loader.setAttribute('data-opened', 'false'), 3000)
+
+document.querySelectorAll('a').forEach(item => {
+    item.setAttribute('href',`/${item.href}`)
+})
