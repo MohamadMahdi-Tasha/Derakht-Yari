f69cbd7d0c2a48889b531291d7bc5f52d7c1edc6
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-12 20:39:26 +0430

    added structure of left side

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -3,8 +3,7 @@ const loader = document.getElementById('loader');
 const swup = new Swup();
 
 // Adding Event Listener Of Load On Window Which Listens To Load Of Full Page Then Closes Loader Modal
-// window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
-setTimeout(() => loader.setAttribute('data-opened', 'false'), 3000)
+window.addEventListener('load', () => loader.setAttribute('data-opened', 'false'))
 
 document.querySelectorAll('a').forEach(item => {
     item.setAttribute('href',`/${item.href}`)
