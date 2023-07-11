40345cdcc9b6b9b147a71752eab1dd85339cc7d4
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-11 14:15:19 +0330

    added scripts of negar page

---
+++ assets/base/js/negarPage.js
@@ -0,0 +1,34 @@
+// Variables
+const negarItems = document.querySelectorAll('.negar-item');
+const middleHeaderImg = document.getElementById('middle-header-img');
+
+// Adding Event Listener Of Load To Window That ..
+window.addEventListener('load', () => {
+    // Variables
+    const firstNegarItem = negarItems[0];
+    const dataURLFirstNegarItem = firstNegarItem.getAttribute('data-url');
+
+    // Setting Src Attribute Of Img In Header Of Middle Section To Value Of 'data-url' Attribute Of First Negar Item
+    middleHeaderImg.src = dataURLFirstNegarItem;
+
+    // Adding Class Of 'active' to First Negar Item
+    firstNegarItem.classList.add('active');
+})
+
+// Adding Event Listener Of Click To Each Nagar Item That ..
+negarItems.forEach((item => {
+    item.addEventListener('click', () => {
+        // Variables
+        const activeNegarItem = document.querySelector('.negar-item.active');
+        const dataURLClickedElement = item.getAttribute('data-url');
+
+        // Adding Class Of 'active' to Clicked Item
+        item.classList.add('active');
+
+        // Removing Class Of 'active' from Active Negar Item
+        activeNegarItem.classList.remove('active');
+
+        // Setting Src Attribute Of Img In Header Of Middle Section To Value Of 'data-url' Attribute Of Clicked Element
+        middleHeaderImg.src = dataURLClickedElement;
+    })
+}))
